34f8bc89bd0865cb8d01130a735c6dc953f73d4b
RiccardoLoPresti <riccardolopresti35@gmail.com>
2022-11-03 15:39:00 +0100

    [fix] img url

--- js/script.js
+++ js/script.js
@@ -18,27 +18,27 @@ createApp({
         {
           nome: "Argentina",
           descrizione: " Il Paese del tango è capace di offrire ai viaggiatori paesaggi straordinari",
-          pic: "argentina.jpg"
+          pic: "./img/argentina.jpg"
         },
         {
           nome: "Chile",
           descrizione: "Preparati per un'avventura senza precedenti da nord, con il deserto più secco del mondo, al sud australe con il ghiaccio eterno e cascate invertite",
-          pic: "chile.jpg"
+          pic: "./img/chile.jpg"
         },
         {
           nome: "Colombia",
           descrizione: "La maggior parte del paese, è coperta di montagne come le Ande",
-          pic: "colombia.jpg"
+          pic: "./img/colombia.jpg"
         },
         {
           nome: "Perù",
           descrizione: "Il Perù è un paese megadiverso con habitat che variano dalle pianure aride della costa del Pacifico, alle vette delle Ande che si estendono da nord a sud-est",
-          pic: "peru.jpg"
+          pic: "./img/peru.jpg"
         },
         {
           nome: "Svezia",
           descrizione: " per i suoi paesaggi mozzafiato, i pittoreschi villaggi di pescatori, la terra delle renne e il sole estivo senza fine",
-          pic: "svezia.jpg"
+          pic: "./img/svezia.jpg"
         }
       ],
       activeImage: 0
