2d54c21e7c6d66525b24a1be6348331d06ed1ce3
RiccardoLoPresti <riccardolopresti35@gmail.com>
2022-11-03 17:31:24 +0100

    [add] autoplay + maouse over/ mouseleave

--- js/script.js
+++ js/script.js
@@ -1,14 +1,3 @@
-/*Descrizione:
-Partendo dal markup della versione svolta in js plain, rifare lo slider ma questa volta usando Vue.
-- al click su una thumb, visualizzare in grande l’immagine corrispondente
-- aggiungere la classe active alla thumb attiva
-Bonus:
-1- applicare l’autoplay allo slider: ogni 3 secondi, cambia immagine automaticamente
-2- quando il mouse va in hover sullo slider, bloccare l’autoplay e farlo riprendere quando esce
-Consigli del giorno:
-- regola d’oro: riciclare ovunque possibile! Questo significa che per la parte di markup possiamo recuperare html e css dell’esercizio svolto qualche giorno fa: è già tutto pronto!
-- il riciclo spesso va a braccetto con le funzioni! Sapendole sfruttare bene, l’esercizio si riduce a poche righe ;)*/
-
 const {createApp} = Vue;
 
 createApp({
@@ -41,7 +30,9 @@ createApp({
           pic: "./img/svezia.jpg"
         }
       ],
-      activeImage: 0
+      activeImage: 0,
+      isOver: false,
+      myPlay: undefined
     }
   },
   methods:{
@@ -49,14 +40,25 @@ createApp({
       this.activeImage = index;
     },
     nextPrev(isNext){
-      if(isNext) this.activeImage++
-      else this.activeImage--;
-      console.log(this.activeImage);
-      if(this.activeImage = 5){
+      isNext ? this.activeImage++ : this.activeImage--;
+
+      if(this.activeImage === 5){
         this.activeImage = 0;
       }else if(this.activeImage < 0){
         this.activeImage = 4;
       }
+    },
+    autoPlay(isNext,isOver){
+      this.stopAutoPlay(isOver)
+      this.myPlay =setInterval(() =>{
+       this.nextPrev(!isNext)
+      },3000)
+    },
+    stopAutoPlay(isOver){
+      if(isOver=true) clearInterval(this.myPlay);
     }
+  },
+  mounted(){
+    this.autoPlay();
   }
 }).mount('#app');
